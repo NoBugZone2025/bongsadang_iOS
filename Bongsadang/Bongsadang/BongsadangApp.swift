f169123bf2f1ddf2222b871295b352b42a789ed8
jihoon <leejh080628@naver.com>
2025-11-05 20:11:20 +0900

    setting :: first commit

---
+++ Bongsadang/Bongsadang/BongsadangApp.swift
@@ -0,0 +1,17 @@
+//
+//  BongsadangApp.swift
+//  Bongsadang
+//
+//  Created by 이지훈 on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BongsadangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bongsadang/Bongsadang/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bongsadang
+//
+//  Created by 이지훈 on 11/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
